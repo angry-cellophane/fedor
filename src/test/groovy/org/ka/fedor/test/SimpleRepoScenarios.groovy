31502753866f8b55cef0045c43c39fc9c656f348
Alexander Kazakov <kazakov.a.s.90@gmail.com>
2016-04-25 01:06:30 +0300

    Tests for several persons added

--- src/test/groovy/org/ka/fedor/test/SimpleRepoScenarios.groovy
+++ src/test/groovy/org/ka/fedor/test/SimpleRepoScenarios.groovy
@@ -7,7 +7,7 @@ import spock.lang.Unroll
 class SimpleRepoScenarios extends Specification implements RepositoryTestUtils {
 
     @Unroll
-    def 'simple test for #repo'() {
+    def 'put a person and get its value from #repo'() {
         given:
         def node = repo.put(object)
         expect:
@@ -16,5 +16,18 @@ class SimpleRepoScenarios extends Specification implements RepositoryTestUtils {
         repo << repos.values()
         object << [new Person(1, 'Alex', 'Rot')] * repos.size()
     }
+
+    @Unroll
+    def 'put several persons and get their values from #repo'() {
+        given:
+        def nodes = persons.collect { repo.put(it) }
+        expect:
+        nodes*.getValue() == persons
+        where:
+        repo << repos.values()
+        persons << [ [new Person(24, 'Alex', 'Rot'), new Person(25, 'Bob', 'Kelso'), new Person(26, 'John', 'Dorian')] ] * repos.size()
+    }
+
+
 
 }
